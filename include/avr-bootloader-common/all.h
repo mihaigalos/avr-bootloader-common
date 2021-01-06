a7144ab60fe6e929a39cd534f63330a656ae30b7
Mihai Galos <mihaigalos@gmail.com>
2021-01-06 11:54:18 +0100

    Add central all.h

---
+++ include/avr-bootloader-common/all.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <avr-bootloader-common/config.h>
+#include <avr-bootloader-common/crc32.h>
+#include <avr-bootloader-common/eeprom.h>
+#include <avr-bootloader-common/flash.h>
+#include <avr-bootloader-common/init.h>
+#include <avr-bootloader-common/io.h>
